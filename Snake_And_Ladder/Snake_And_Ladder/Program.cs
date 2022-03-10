09dc98f20df55134aeb0a8a6970f93a6f82bff77
yburkul <yburkul123@gmail.com>
2022-03-10 17:20:29 +0530

    Added:Start Postion

---
+++ Snake_And_Ladder/Snake_And_Ladder/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Snake_and_Ladder_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game!");
+            int PlayerPosition = 0;
+            Console.WriteLine("Position of Player: " + PlayerPosition);
+
+        }
+
+    }
+
+}
